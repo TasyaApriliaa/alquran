5c98011f538690ae3c9d6173c771612ca160ccc3
TasyaApriliaa <12050123544@students.uin-suska.ac.id>
2022-05-09 01:23:43 +0700

    1

--- src/views/JuzView.vue
+++ src/views/JuzView.vue
@@ -9,58 +9,58 @@ export default {
       loading: true,
       juzke: ref([]),
       bacajuz: ref([]),
-
-    }
+    };
   },
 
-  watch:{
-    $route(){
-      this.getJuzke()
-      this.getBacaJuz()
-    }
+  watch: {
+    $route() {
+      this.getJuzke();
+      this.getBacaJuz();
+    },
   },
 
   mounted() {
-    this.getJuzke()
-    this.getBacaJuz()
+    this.getJuzke();
+    this.getBacaJuz();
   },
 
   methods: {
     getJuzke() {
-      axios.get('https://api.quran.com/api/v4/juzs/' + this.$route.params.id)
-        .then(response => {
-          this.juzke = response.data.juz
+      axios
+        .get("https://api.quran.com/api/v4/juzs/" + this.$route.params.id)
+        .then((response) => {
+          this.juzke = response.data.juz;
         })
-        .catch(error => {
-          console.log(error)
-          this.error = true
+        .catch((error) => {
+          console.log(error);
+          this.error = true;
         })
-        .finally(() => this.loading = false)
+        .finally(() => (this.loading = false));
     },
     getBacaJuz() {
-      axios.get('https://api.quran.com/api/v4/quran/verses/uthmani?juz_number=' + this.$route.params.id )
-        .then(response => {
-          this.bacajuz = response.data.verses
-
+      axios
+        .get(
+          "https://api.quran.com/api/v4/quran/verses/uthmani?juz_number=" +
+            this.$route.params.id
+        )
+        .then((response) => {
+          this.bacajuz = response.data.verses;
         })
-        .catch(error => {
-          console.log(error)
-          this.error = true
+        .catch((error) => {
+          console.log(error);
+          this.error = true;
         })
-        .finally(() => this.loading = false)
-    }
-
-  }
-}
+        .finally(() => (this.loading = false));
+    },
+  },
+};
 </script>
 
 <template>
   <div class="text-center mt-5">
     <h1>--- Juz {{ $route.params.id }} ---</h1>
     <h4 v-for="juzs in bacajuz" :key="juzs.id" class="text-end mt-5">
-      {{juzs.text_uthmani}} {{juzs.verse_key}}
+      {{ juzs.text_uthmani }} {{ juzs.verse_key }}
     </h4>
   </div>
-
 </template>
-
